2abbc8458093cf88d159fcd984fd175e0b97d8be
Dave Steinberg <dave@steinbergcomputing.com>
2023-09-17 22:14:21 +0000

    allow for newer provider versions

--- versions.tf
+++ versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~>3.25"
+      version = ">= 3.25"
     }
     google = {
       source = "hashicorp/google"
-      version = "~> 3.3"
+      version = ">= 3.3"
     }
   }
   required_version = ">= 0.13"
